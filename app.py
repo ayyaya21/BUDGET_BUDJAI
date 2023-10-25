a37fd9411e83188f0c2d813e6af64fcd4f1dc945
phufaphu <Vincent0137z@gmail.com>
2023-10-25 12:26:42 +0700

    merge commit

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 #หน้าสมัครแบบ ทดลอง
 @app.route("/register")
@@ -16,6 +19,7 @@ def register():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
